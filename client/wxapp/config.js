9cd5d2705020574868e93cd012f0fcc9ae6d0a93
jianglang <643546122@qq.com>
2020-08-15 20:30:20 +0800

    提交:

--- client/wxapp/config.js
+++ client/wxapp/config.js
@@ -6,7 +6,7 @@ module.exports = {
   domain: "http://106.14.122.191:8080/food", //测试
   domainUrl:'http://106.14.122.191:8080/food',
   shareData: {
-    title: 'Latelier Martell',
+    title: '嘬一口炸鸡',
     path: '/pages/index/index',
     imageUrl: '/images/share.jpg'
   }
